a19dc80fdf111a7eeb329d7209cb5978efbcce95
SatoshiHaramura <satoshi.haramura@gmail.com>
2022-02-24 01:28:26 +0900

    ESLintをパスする

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,9 +1,9 @@
-for(let number = 1; number <= 20; number++) {
-  if ((number % 3 == 0) && (number % 5 == 0)) {
+for (let number = 1; number <= 20; number++) {
+  if ((number % 3 === 0) && (number % 5 === 0)) {
     console.log('FizzBuzz')
-  } else if (number % 3 == 0) {
+  } else if (number % 3 === 0) {
     console.log('Fizz')
-  } else if (number % 5 == 0) {
+  } else if (number % 5 === 0) {
     console.log('Buzz')
   } else {
     console.log(number)
