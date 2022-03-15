76d459a0656b721373a89596154a5c707feb40b4
SatoshiHaramura <satoshi.haramura@gmail.com>
2022-03-15 15:08:02 +0900

    Refactor index.js

--- 03.asynchronous/index.js
+++ 03.asynchronous/index.js
@@ -1,16 +1,21 @@
-async function fetchFbcApp () {
+async function main () {
   try {
-    const response = await fetch('https://bootcamp.fjord.jp/')
-
-    if (response.ok) {
-      const data = await response.text()
-      console.log(data)
-    } else {
-      console.error('エラーレスポンス', response)
-    }
-  } catch (error) {
-    console.error(error)
+    const text = await fetchFbcApp()
+    console.log(text)
+  } catch (e) {
+    console.error(e)
   }
 }
 
-fetchFbcApp()
+async function fetchFbcApp () {
+  const response = await fetch('https://bootcamp.fjord.jp/')
+
+  if (response.ok) {
+    const text = await response.text()
+    return text
+  } else {
+    throw new Error(`${response.status}`)
+  }
+}
+
+main()
