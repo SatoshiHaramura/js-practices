42dcaee819ff63d834d53fa388c0f2a6ee5ad0c8
SatoshiHaramura <satoshi.haramura@gmail.com>
2022-03-25 16:19:47 +0900

    Refactor memo.js for classing

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const argv = require('minimist')(process.argv.slice(2))
+const arg = require('minimist')(process.argv.slice(2))
 
 const { Select } = require('enquirer')
 
@@ -7,56 +7,76 @@ const memoFile = 'memo.json'
 const json = fs.readFileSync(memoFile, 'utf8')
 const memos = JSON.parse(json)
 
-if (argv.l) {
-  memos.forEach((value, index) => { console.log(value[0]) })
-} else if (argv.r) {
-  const prompt = new Select({
-    message: 'Choose a note you want to see:',
-    choices: memos.map((val, index) => { return { name: val[0], value: index } }),
-    result () {
-      return this.focused.value
-    }
-  })
+class Memo {
+  append () {
+    process.stdin.setEncoding('utf8')
 
-  prompt.run()
-    .then((answer) => {
-      let index = answer
-      if (typeof answer === 'string') {
-        index = 0
-      }
-      memos[index].forEach(value => console.log(value))
+    const lines = []
+
+    const reader = require('readline').createInterface({
+      input: process.stdin
     })
-    .catch(console.error)
-} else if (argv.d) {
-  const prompt = new Select({
-    message: 'Choose a note you want to delete:',
-    choices: memos.map((val, index) => { return { name: val[0], value: index } }),
-    result () {
-      return this.focused.value
-    }
-  })
 
-  prompt.run()
-    .then((answer) => {
-      memos.splice(answer, 1)
+    reader.on('line', (line) => {
+      lines.push(line)
+    })
+
+    reader.on('close', () => {
+      memos.push(lines)
       fs.writeFileSync(memoFile, JSON.stringify(memos))
     })
-    .catch(console.error)
+  }
+
+  list () {
+    memos.forEach((value, index) => { console.log(value[0]) })
+  }
+
+  refer () {
+    const prompt = new Select({
+      message: 'Choose a note you want to see:',
+      choices: memos.map((val, index) => { return { name: val[0], value: index } }),
+      result () {
+        return this.focused.value
+      }
+    })
+
+    prompt.run()
+      .then((answer) => {
+        let index = answer
+        if (typeof answer === 'string') {
+          index = 0
+        }
+        memos[index].forEach(value => console.log(value))
+      })
+      .catch(console.error)
+  }
+
+  delete () {
+    const prompt = new Select({
+      message: 'Choose a note you want to delete:',
+      choices: memos.map((val, index) => { return { name: val[0], value: index } }),
+      result () {
+        return this.focused.value
+      }
+    })
+
+    prompt.run()
+      .then((answer) => {
+        memos.splice(answer, 1)
+        fs.writeFileSync(memoFile, JSON.stringify(memos))
+      })
+      .catch(console.error)
+  }
+}
+
+const memo = new Memo(arg)
+
+if (arg.l) {
+  memo.list()
+} else if (arg.r) {
+  memo.refer()
+} else if (arg.d) {
+  memo.delete()
 } else {
-  process.stdin.setEncoding('utf8')
-
-  const lines = []
-
-  const reader = require('readline').createInterface({
-    input: process.stdin
-  })
-
-  reader.on('line', (line) => {
-    lines.push(line)
-  })
-
-  reader.on('close', () => {
-    memos.push(lines)
-    fs.writeFileSync(memoFile, JSON.stringify(memos))
-  })
+  memo.append()
 }
